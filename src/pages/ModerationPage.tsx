7cbd6f88eefbc64c675d5ece3c5edd753fe909a8
Valimp <valentinchauvet.contact@gmail.com>
2024-03-25 13:19:14 +0100

    fix: fit fetch url to get tickets

--- src/pages/ModerationPage.tsx
+++ src/pages/ModerationPage.tsx
@@ -15,7 +15,7 @@ export default function ImageModerationPage() {
 
     useEffect(() => {
         // send get request to api to get tickets and set Tickets to the response
-        axios.get(`${import.meta.env.VITE_API_URL}/tickets`).then((res) => {
+        axios.get(`${import.meta.env.VITE_API_URL}/tickets?status=open`).then((res) => {
             setTickets(res.data.tickets)
         })
     }, [])
