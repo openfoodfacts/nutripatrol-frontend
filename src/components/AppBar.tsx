e914694a26976f95464ef597339be99b47b8cb00
Valimp <valentinchauvet.contact@gmail.com>
2024-04-15 12:21:30 +0200

    fix: nutripatrol redirect

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -44,7 +44,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="#"
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
@@ -119,7 +119,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="#"
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
