e99a7bef3123bbd8f386e0854248a120a5d38e68
Valimp <valentinchauvet.contact@gmail.com>
2024-04-15 15:13:27 +0200

    feat: add loader

--- src/pages/ImageModerationPage.tsx
+++ src/pages/ImageModerationPage.tsx
@@ -18,46 +18,56 @@ interface Ticket {
 export default function ImageModerationPage() {
 
     const [Tickets, setTickets] = useState<Ticket[]>([])
+    const [isLoading, setIsLoading] = useState<Boolean>(true)
 
     useEffect(() => {
         // send get request to api to get tickets and set Tickets to the response
         axios.get(`${import.meta.env.VITE_API_URL}/tickets?type_=image&status=open`).then((res) => {
             setTickets(res.data)
+            setIsLoading(false)
         }).catch((err) => {
             console.error(err)
         })
     }, [])
         
-    if (Tickets.length === 0) {
-        return (
-            <div style={{position: "absolute", width: "100vw", height: "100vh", zIndex: "-10", color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center"}}>
-                <h2 style={{fontSize: '1.4rem', margin: "2rem 0"}}>No new ticket </h2> 
-            </div>
-        )
-    }
     return (
         <>
-            <h2 style={{width: "100vw", zIndex: "-10", color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center", padding: 20}}>
-                Image moderation
-            </h2>
-            <div style={{ height: 400, width: '100%' }}>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Image</TableCell>
-                            <TableCell align="center">Date</TableCell>
-                            <TableCell align="center">Actions</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {Tickets.map((ticket) => (
-                        <ImageTicket ticket={ticket} />
-                    ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            </div>
+            {
+                // if the page is loading, display a loading message
+                isLoading ? (
+                    <div>Is loading...</div>
+                ) : (
+                    Tickets.length === 0 ? (
+                        <div style={{position: "absolute", width: "100vw", height: "100vh", zIndex: "-10", color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center"}}>
+                            <h2 style={{fontSize: '1.4rem', margin: "2rem 0"}}>No new ticket </h2> 
+                        </div>
+                    ) : (
+                        <div>
+                            <h2 style={{width: "100vw", zIndex: "-10", color: '#281900', display: 'flex',flexDirection: "column", alignItems: "center", justifyContent:"center", padding: 20}}>
+                                Image moderation
+                            </h2>
+                            <div style={{ height: 400, width: '100%' }}>
+                            <TableContainer component={Paper}>
+                                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center">Image</TableCell>
+                                            <TableCell align="center">Date</TableCell>
+                                            <TableCell align="center">Actions</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                    {Tickets.map((ticket) => (
+                                        <ImageTicket ticket={ticket} />
+                                    ))}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            </div>
+                         </div>
+                    )
+                )
+            }
         </>
     )
 }
