54644c531e1edca75f4d2e43f81da2cbfaa3e1b7
Valimp <valentinchauvet.contact@gmail.com>
2024-04-18 09:56:11 +0200

    Boxify div in tickets

--- src/components/ImageTicket.tsx
+++ src/components/ImageTicket.tsx
@@ -2,6 +2,7 @@ import TableCell from '@mui/material/TableCell';
 import TableRow from '@mui/material/TableRow';
 import Date from './Date'
 import TicketsButtons from './TicketsButtons'
+import Box from '@mui/material/Box';
 
 export default function ImageTicket({ticket}: {ticket: any}) {
     
@@ -11,7 +12,7 @@ export default function ImageTicket({ticket}: {ticket: any}) {
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
         >
             <TableCell align="center">
-                <div style={{ display: 'flex', gap: '10px', alignItems: 'center', justifyContent: 'center'}}>
+                <Box sx={{ display: 'flex', gap: '10px', alignItems: 'center', justifyContent: 'center'}}>
                     <img 
                         src={ticket.url} 
                         // src='https://images.openfoodfacts.net/images/products/327/408/000/5003/1.400.jpg'
@@ -19,7 +20,7 @@ export default function ImageTicket({ticket}: {ticket: any}) {
                         width={100}
                         height={100}
                     />
-                </div>
+                </Box>
             </TableCell>
             <Date created_at={ticket.created_at} />
             <TableCell align="center"><TicketsButtons barcode={ticket.barcode} id={ticket.id} /></TableCell>
