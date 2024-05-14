f105d94088666de0443407929714f6a900cf41d0
Valimp <valentinchauvet.contact@gmail.com>
2024-05-14 13:16:47 +0200

    remove log

--- src/pages/ImageModerationPage.tsx
+++ src/pages/ImageModerationPage.tsx
@@ -30,9 +30,7 @@ export default function ImageModerationPage() {
     useEffect(() => {
         // send get request to api to get tickets and set Tickets to the response
         const url = `${import.meta.env.VITE_API_URL}/tickets?type_=image&status=open&page=${currentPage}&page_size=3`
-        axios.get(url).then((res) => {
-            console.log(res.data.tickets);
-            
+        axios.get(url).then((res) => {            
             setTickets(res.data.tickets)
             setMaxPage(res.data.max_page)
             setIsLoading(false)
